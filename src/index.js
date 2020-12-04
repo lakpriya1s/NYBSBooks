e42bb69f013cd143138dfd1925b43b0850a6169a
Lakpriya <lakpriyasenevirathna@gmail.com>
2020-12-04 17:40:34 +0530

    final

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
+import 'jquery/dist/jquery.slim.min.js'
+import 'popper.js/dist/umd/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDOM.render(
   <App/>,
