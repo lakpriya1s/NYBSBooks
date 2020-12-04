4170ebd2ba7348f8c04a7c0517fe1c5bfc6675c2
Lakpriya <lakpriyasenevirathna@gmail.com>
2020-12-04 23:24:45 +0530

    readme7

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -2,19 +2,54 @@ import React from 'react';
 
 const AboutUs = () => {
     return ( <div>
-        <h2><strong>Developed by: </strong>Lakpriya Senevirathna</h2>
-        <p>I am Lakpriya Senevirathana, an
-        undergraduate student from
-        University of Peradeniya. And I am a
-        freelance android application
-        developer with full application
-        development life cycle experience
-        including designing, development
-        and implementing over 2+ years of
-        the application development field. I
-        also have rich experience in telco
-        application development and web
-        site design.</p>
+        <h2>NYBS BOOKS</h2>
+        <h3>NYBS BOOKS</h3>
+
+        <p>This is a react based-web application to show the New York Times best-selling books list.</p>
+
+        <img src="https://github.com/lakpriya1s/NYBSBooks/blob/master/src/logo.svg" alt="logo" width="150"></img>
+
+
+<p> Go to [NYBS Books web app](https://nybsbooks.netlify.app)</p>
+
+<h3> Books API </h3>
+
+<p> Used the web api of New York Times Books API [https://developer.nytimes.com/](https://developer.nytimes.com/) </p>
+
+<h3> How this app works </h3>
+
+<p> In the home page, we will see the best sellers list names in New York. Then we can select one of them to see books on that list.
+
+In the top 5 books section, we can see the top 5 books for all the Best Sellers lists.
+
+By mouse hovering on a book, We can see more details about the selected book, and clicking on the buy button we can order the book from amazon.com. By clicking on the review button on a book, we can see book reviews that selected book got.</p>
+
+<h2>Screenshots</h2>
+
+<h3>Desktop</h3> 
+
+<img src="https://github.com/lakpriya1s/NYBSBooks/blob/master/screenshots/image-d-books.png" alt="desktop" width="500"></img>
+
+<img src="https://github.com/lakpriya1s/NYBSBooks/blob/master/screenshots/image-d.png" alt="desktop" width="500"></img>
+
+<img src="https://github.com/lakpriya1s/NYBSBooks/blob/master/screenshots/image-d-revs.png" alt="desktop" width="500"></img>
+
+<h3>Mobile</h3>
+
+<img src="https://github.com/lakpriya1s/NYBSBooks/blob/master/screenshots/mobile1.jpg" alt='mobile' width="200"></img>
+<img src="https://github.com/lakpriya1s/NYBSBooks/blob/master/screenshots/mobile2.jpg" alt='mobile' width="200"></img>
+
+<h3>Credits</h3> 
+
+<p>Thanks to [Netlify](https://www.netlify.com/) for hosting.</p>
+
+<h3>Developer</h3>
+
+<p>I am Lakpriya Senevirathana, an undergraduate student from University of Peradeniya. And I am a freelance android application developer with full application development life cycle experience including designing, development and implementing over 2+ years of the application development field. I also have rich experience in telco application development and web site design.
+
+- [linkedIn](https://www.linkedin.com/in/lakpriyasenevirathna/)
+- [gmail](lakpriya1@yahoo.com)</p> 
+
     </div>
      );
 }
